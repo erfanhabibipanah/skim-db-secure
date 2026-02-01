0b1fd546615839723626978be06e8682f4191e22
Jaroslaw Zola <jaroslaw.zola@hush.com>
2026-02-01 11:52:53 -0500

    Shebang fix.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 PROJECT=""
 JOBS=8
--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 export SPDLOG_LEVEL=debug
 ../release/bin/skimdb-index-create -i reference/sequences -o test.skimdb -k 15 -s 9 -t 0
 echo "AAAATATATAATAAA AACGGTCCTAAGGTA" | ../release/bin/skimdb-index-query -i test.skimdb
