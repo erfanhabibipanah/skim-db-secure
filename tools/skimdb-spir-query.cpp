e30372accc7da7b313c76032e3f41f12c21ad7d9
Jaroslaw Zola <jaroslaw.zola@hush.com>
2026-02-02 15:15:16 -0500

    SPIR client fixes.

--- tools/skimdb-spir-query.cpp
+++ tools/skimdb-spir-query.cpp
@@ -21,6 +21,7 @@ auto main(int argc, char* argv[]) -> int {
   unsigned int logq = 64;
   std::size_t n = 1000;
   double sigma = 6.4;
+  bool verbose = false;
 
   try {
     cxxopts::Options options(argv[0]);
@@ -31,6 +32,7 @@ auto main(int argc, char* argv[]) -> int {
       ("q,logq", "log of cypher modulus q", cxxopts::value<unsigned int>(logq)->default_value(std::to_string(logq)))
       ("n", "secret size", cxxopts::value<std::size_t>(n)->default_value(std::to_string(n)))
       ("s,sigma", "variance of error distribution", cxxopts::value<double>(sigma)->default_value(std::to_string(sigma)))
+      ("v,verbose", "print recovered labels", cxxopts::value<bool>(verbose)->default_value(std::to_string(verbose)))
       ("h,help", "print this help");
 
     auto opt_res = options.parse(argc, argv);
@@ -70,6 +72,9 @@ auto main(int argc, char* argv[]) -> int {
     return -1;
   }
 
+  auto [k, s, t] = db.parameters();
+  log->info("index loaded, [k={}, s={}, t={}]", k, s, t);
+
   log->info("building spir server state...");
 
   auto setup = skim::spir::make_server(std::move(db), logp, logq, n, sigma);
@@ -87,44 +92,56 @@ auto main(int argc, char* argv[]) -> int {
   skim::spir::spir_client_state client_state{server_state.skim_parameters(), server_state.skim_metadata(),
                                              server_state.spir_parameters(), server_state.hint_c()};
 
+  log->info("client ready for queries...");
 
-  log->info("creating query for kmer AACGGTCCTAAGGTA...");
+  std::string q{};
 
-  auto query = client_state.prepare_query("AACGGTCCTAAGGTA");
+  while (std::cin >> q && std::cin && !std::cin.eof()) {
+    auto query = client_state.prepare_query(q);
 
-  if (!query) {
-    log->error("could not create query: {}", query.error());
-    return -1;
+    if (!query) {
+      log->warn("could not create query: {}", query.error());
+      continue;
+    }
+
+    auto query_state = query.value();
+
+    log->info("submitting query...");
+
+    auto answer = server_state.answer(query_state.qu_vec);
+
+    if (!answer) {
+      log->warn("could not get answer: {}", answer.error());
+      continue;
+    }
+
+    auto answer_vec = answer.value();
+
+    log->info("recovering result...");
+
+    auto recover_mat = client_state.recover(answer_vec, query_state);
+
+    if (!recover_mat) {
+      log->warn("could not recover result: {}", recover_mat.error());
+      continue;
+    }
+
+    auto res_mat = recover_mat.value();
+
+    if (verbose) {
+      log->info("query results:");
+
+      for (auto label : client_state.result(res_mat)) {
+        log->info("  {}", label);
+      }
+    } else {
+      log->info("got {} labels", std::ranges::distance(client_state.result(res_mat)));
+    }
   }
-  auto query_state = query.value();
 
-  log->info("submitting query...");
+  std::cout << "\n";
 
-  auto answer = server_state.answer(query_state.qu_vec);
-
-  if (!answer) {
-    log->error("could not get answer: {}", answer.error());
-    return -1;
-  }
-
-  auto answer_vec = answer.value();
-
-  log->info("recovering result...");
-
-  auto recover_mat = client_state.recover(answer_vec, query_state);
-
-  if (!recover_mat) {
-    log->error("could not recover result: {}", recover_mat.error());
-    return -1;
-  }
-
-  auto res_mat = recover_mat.value();
-
-  log->info("query results:");
-
-  for (auto label : client_state.result(res_mat)) {
-    log->info("  {}", label);
-  }
+  log->info("done!");
 
   return 0;
 }
