7729bc9554a338cb4414eda84884c8c9a369ef3b
Jaroslaw Zola <jaroslaw.zola@hush.com>
2026-02-04 15:23:29 -0500

    Smode code cleanup.

--- tools/skimdb-spir-dbsize.cpp
+++ tools/skimdb-spir-dbsize.cpp
@@ -17,27 +17,27 @@ namespace fs = std::filesystem;
 
 
 auto main(int argc, char* argv[]) -> int {
-	std::string in = "";
+  std::string in = "";
   unsigned int logp = 16;
 
-	try {
-		cxxopts::Options options(argv[0]);
+  try {
+    cxxopts::Options options(argv[0]);
 
-		options.add_options()
-			("i,input", "input database file", cxxopts::value<std::string>(in))
+    options.add_options()
+      ("i,input", "input database file", cxxopts::value<std::string>(in))
       ("p,logp", "log of text modulus p", cxxopts::value<unsigned int>(logp)->default_value(std::to_string(logp)))
-			("h,help", "print this help");
+      ("h,help", "print this help");
 
-		auto opt_res = options.parse(argc, argv);
+    auto opt_res = options.parse(argc, argv);
 
-		if((opt_res.unmatched().size() != 0) || opt_res.count("help")) {
-			std::cout << options.help() << std::endl;
-			return 0;
-		}
-	} catch (const cxxopts::exceptions::exception& e) {
-		std::cerr << e.what() << std::endl;
-		return -1;
-	}
+    if ((opt_res.unmatched().size() != 0) || opt_res.count("help")) {
+      std::cout << options.help() << std::endl;
+      return 0;
+    }
+  } catch (const cxxopts::exceptions::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return -1;
+  }
 
   spdlog::cfg::load_env_levels();
   auto log = spdlog::stdout_color_mt("spirdb-precompute-size");
@@ -64,16 +64,18 @@ auto main(int argc, char* argv[]) -> int {
 
   skim::skimdb db;
   auto res = db.load(dir);
-  
+
   if (!res) {
     log->error("could not load {}, error: {}!", in, res.error());
     return -1;
   }
-	
+
   auto data = std::move(db).explode().data;
-	auto kmer_count = data.size();
-	std::uint64_t max_rle = 0;
-	for (const auto& entry : data) {
+  auto kmer_count = data.size();
+
+  std::uint64_t max_rle = 0;
+
+  for (const auto& entry : data) {
     max_rle = std::max(max_rle, entry.length());
   }
 
@@ -82,7 +84,8 @@ auto main(int argc, char* argv[]) -> int {
 
   std::uint64_t bytes_per_rle = 2 * max_rle;
   std::uint64_t bytes_per_block = logp / 8;
-  std::uint64_t blocks_per_rle = (bytes_per_rle % bytes_per_block) ? (bytes_per_rle / bytes_per_block) + 1 : (bytes_per_rle / bytes_per_block);
+  std::uint64_t blocks_per_rle =
+      (bytes_per_rle % bytes_per_block) ? (bytes_per_rle / bytes_per_block) + 1 : (bytes_per_rle / bytes_per_block);
 
   log->info("blocks required per RLE: {}", blocks_per_rle);
 
@@ -92,8 +95,6 @@ auto main(int argc, char* argv[]) -> int {
   std::uint64_t sqrt_N = rles_per_side * blocks_per_rle;
 
   log->info("sqrt(N) = {}, N = {}", sqrt_N, sqrt_N * sqrt_N);
-	
+
   return 0;
 }
-
-
