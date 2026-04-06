1e67edfd49d7a4aabf056143a2e3d77f0f22d617
Sam <sranders@buffalo.edu>
2026-04-06 15:39:12 -0400

    updating test script

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export SPDLOG_LEVEL=debug
-../release/bin/skimdb-index-create -i reference/sequences -o test.skimdb -k 15 -s 9 -t 0
-echo "AAAATATATAATAAA AACGGTCCTAAGGTA" | ../release/bin/skimdb-index-query -i test.skimdb
+#../release/bin/skimdb-index-create -i reference/sequences -o test.skimdb -k 15 -s 0 -t 0
+../release/bin/skimdb-index-create -i reference/sequences -l reference/file2taxid -o test.skimdb -k 15 -s 0 -t 0
+echo -e "AATGAATAATGGAAC\nAAAATATATAATAAA\nAACGGTCCTAAGGTA" | ../release/bin/skimdb-index-query -i test.skimdb
