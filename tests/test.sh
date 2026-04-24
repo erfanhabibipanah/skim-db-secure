536c2e8179b34598c122a09f3bc9bfd3299cf704
Jaroslaw Zola <jaroslaw.zola@hush.com>
2026-04-23 20:52:18 -0400

    sync

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 #export SPDLOG_LEVEL=debug
+
 #../release/bin/skimdb-index-create -i reference/sequences -o test.skimdb -k 15 -s 0 -t 0
 #../release/bin/skimdb-index-create -i reference/sequences -l reference/file2strain -o test.skimdb -k 15 -s 0 -t 0
 ../release/bin/skimdb-index-create -i reference/sequences -l reference/file2taxid -o test.skimdb -k 15 -s 0 -t 0
